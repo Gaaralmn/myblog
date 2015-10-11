a28b5cc07c3a5ff4a00b91a77a1e9210199ca969
Gaaralmn <Gaaralmn@gmail.com>
2015-10-11 17:06:52 -0400

    Version_1.0

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse  #What is this?
 
 # Create your models here.
 class Article(models.Model) :
@@ -7,8 +8,14 @@ class Article(models.Model) :
     date_time = models.DateTimeField(auto_now_add = True)
     content = models.TextField(blank = True, null = True)
     # What is blank = True and null = True ?
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://127.0.0.1:8000%s" % path
+
     def __unicode__(self) :
         return self.title
+
     # Why there is a Meta here ?
     class Meta:
         ordering = ['-date_time']
